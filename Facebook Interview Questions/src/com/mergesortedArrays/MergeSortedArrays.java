a5271f505446aecc3ed48864441243350f1475dd
Nisarg Chitaliya <chitaliya.nisarg4@gmail.com>
2019-01-02 23:09:05 -0500

    implemented sorted arrays

---
+++ Facebook Interview Questions/src/com/mergesortedArrays/MergeSortedArrays.java
@@ -0,0 +1,32 @@
+package com.mergesortedArrays;
+
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+	
+	 public static void merge(int[] nums1, int m, int[] nums2, int n) 
+	 {
+		 int i = m-1, j = n -1, k = m + n -1;
+		 while(i >= 0 && j >=0) 
+		 {
+			 nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+		 }
+		 while(j >= 0)
+			 nums1[k--] = nums2[j--];
+	 }
+	
+	public static void main(String args[])
+	{
+		
+		int nums1[] = {1,2,3,0,0,0};
+		int m = 3;
+		
+		int nums2[] = {2,5,6};
+		int n = 3;
+		
+		merge(nums1,m,nums2,n);
+		
+		System.out.println(Arrays.toString(nums1));
+	}
+
+}
